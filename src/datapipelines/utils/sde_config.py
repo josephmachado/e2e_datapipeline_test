7c188883ada44daf37ca4c44e08000c3feb80946
JosephMachado <joseph.machado.professional@gmail.com>
2021-11-20 08:57:30 -0500

    use local env file

--- src/datapipelines/utils/sde_config.py
+++ src/datapipelines/utils/sde_config.py
@@ -1,59 +1,58 @@
-import os
 from dataclasses import dataclass
 from typing import Optional
 
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-load_dotenv()
+config = dotenv_values(".env.local")
 
 
 @dataclass
 class SFTPConnection:
-    username: str
-    password: str
-    host: str
+    username: Optional[str]
+    password: Optional[str]
+    host: Optional[str]
     port: int = 22
 
 
 def get_sftp_creds() -> SFTPConnection:
     return SFTPConnection(
-        username=os.getenv('SFTP_USERNAME', ''),
-        password=os.getenv('SFTP_PASSWORD', ''),
-        host=os.getenv('SFTP_HOST', ''),
+        username=config.get('SFTP_USERNAME', ''),
+        password=config.get('SFTP_PASSWORD', ''),
+        host=config.get('SFTP_HOST', ''),
         port=22,
     )
 
 
 @dataclass
 class AWSCloudConnection:
-    aws_access_key_id: str
-    aws_secret_access_key: str
-    region_name: str
+    aws_access_key_id: Optional[str]
+    aws_secret_access_key: Optional[str]
+    region_name: Optional[str]
     endpoint_url: Optional[str]
 
 
 def get_aws_connection() -> AWSCloudConnection:
     return AWSCloudConnection(
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', ''),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', ''),
-        region_name=os.getenv('AWS_REGION_NAME', ''),
-        endpoint_url=os.getenv('AWS_ENDPOINT_URL', ''),
+        aws_access_key_id=config.get('AWS_ACCESS_KEY_ID', ''),
+        aws_secret_access_key=config.get('AWS_SECRET_ACCESS_KEY', ''),
+        region_name=config.get('AWS_REGION_NAME', ''),
+        endpoint_url=config.get('AWS_ENDPOINT_URL', ''),
     )
 
 
 @dataclass
 class DBConnection:
-    db: str
-    user: str
-    password: str
-    host: str
+    db: Optional[str]
+    user: Optional[str]
+    password: Optional[str]
+    host: Optional[str]
     port: int = 5432
 
 
 def get_warehouse_connection() -> DBConnection:
     return DBConnection(
-        db=os.getenv('WAREHOUSE_DB', ''),
-        user=os.getenv('WAREHOUSE_USER', ''),
-        password=os.getenv('WAREHOUSE_PASSWORD', ''),
-        host=os.getenv('WAREHOUSE_HOST', ''),
+        db=config.get('WAREHOUSE_DB', ''),
+        user=config.get('WAREHOUSE_USER', ''),
+        password=config.get('WAREHOUSE_PASSWORD', ''),
+        host=config.get('WAREHOUSE_HOST', ''),
     )
